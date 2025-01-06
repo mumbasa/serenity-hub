8dd27cbdd37a048af5632d03ac3bf703442a2967
Bryan Laryea <bry_lar@yahoo.com>
2025-01-06 00:37:00 +0000

    added aadta

--- src/main/java/com/serenity/integration/repository/DoctorRepository.java
+++ src/main/java/com/serenity/integration/repository/DoctorRepository.java
@@ -1,5 +1,6 @@
 package com.serenity.integration.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,5 @@ import com.serenity.integration.models.Doctors;
 
 public interface DoctorRepository extends JpaRepository<Doctors,Long>{
 public Doctors findByExternalId(String id);
-public Optional<Doctors> NationalMobileNumber(String mobile);
+public List<Doctors> NationalMobileNumber(String mobile);
 }
