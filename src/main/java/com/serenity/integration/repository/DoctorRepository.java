95a0778c40d4e80220500461bd8e731f034fbf79
Bryan Laryea <bry_lar@yahoo.com>
2025-01-08 16:51:17 +0000

    added his visit thread

--- src/main/java/com/serenity/integration/repository/DoctorRepository.java
+++ src/main/java/com/serenity/integration/repository/DoctorRepository.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.serenity.integration.models.Doctors;
 
 public interface DoctorRepository extends JpaRepository<Doctors,Long>{
 public Optional<Doctors> findByExternalId(String id);
 public List<Doctors> NationalMobileNumber(String mobile);
+@Query(value = "select * from doctors where externalsystem ='his'",nativeQuery = true)
+public List<Doctors> findHisPractitioners();
 }
