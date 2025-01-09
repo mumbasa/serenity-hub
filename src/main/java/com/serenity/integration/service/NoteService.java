0ea9a0c92b9162edb1856e53bc67bda8ba10026d
Bryan Laryea <bry_lar@yahoo.com>
2025-01-09 16:04:26 +0000

    adfad

--- src/main/java/com/serenity/integration/service/NoteService.java
+++ src/main/java/com/serenity/integration/service/NoteService.java
@@ -445,7 +445,7 @@ private EncounterNote createEncounterNote(ResultSet rs, PatientData patientData,
 
     public void chiefThreads() {
 
-        int rows = 59098;//9;
+        int rows = 5909;//89;
         Map<String, PatientData> mps = patientRepository.findAll().stream()
                 .collect(Collectors.toMap(e -> e.getExternalId(), e -> e));
         Map<String, String> doc = doctorRepository.findHisPractitioners().stream()
