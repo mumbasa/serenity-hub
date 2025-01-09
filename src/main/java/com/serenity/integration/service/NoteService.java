63a54ffd651158c2820cb183c2b97c5433de32c0
Bryan Laryea <bry_lar@yahoo.com>
2025-01-09 13:52:47 +0000

    adfad

--- src/main/java/com/serenity/integration/service/NoteService.java
+++ src/main/java/com/serenity/integration/service/NoteService.java
@@ -419,13 +419,13 @@ public class NoteService {
 
     public void chiefThreads() {
 
-        int rows = 590989;
+        int rows = 59098;//9;
         Map<String, PatientData> mps = patientRepository.findAll().stream()
                 .collect(Collectors.toMap(e -> e.getExternalId(), e -> e));
         Map<String, String> doc = doctorRepository.findHisPractitioners().stream()
                 .collect(Collectors.toMap(e -> e.getExternalId(), e -> e.getSerenityUUid()));
 
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
         try {
             List<Future<Integer>> futures = executorService.invokeAll(submitTask2(rows, 1000, mps, doc));
             for (Future<Integer> future : futures) {
