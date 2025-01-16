ae87c059c474eb6f51b6b41e903c6bbd973451cf
Bryan Laryea <bry_lar@yahoo.com>
2025-01-16 09:53:28 +0000

    added encounter threads

--- src/main/java/com/serenity/integration/repository/VisitRepository.java
+++ src/main/java/com/serenity/integration/repository/VisitRepository.java
@@ -15,7 +15,7 @@ public interface VisitRepository extends JpaRepository<Visits,Long>{
     List<Visits> getfirst100k(int offset,int limit);
   
 
-    @Query(value = "SELECT * FROM visits OFFSET ?1  LIMIT 1000",nativeQuery = true)
+    @Query(value = "SELECT * FROM visits  where assignedtoid is not null OFFSET ?1  LIMIT 1000",nativeQuery = true)
     List<Visits> getfirst100k(int offset);
   
     @Query(value = "select * from visits where createdat =?1 and hisnumber =?2 and assignedtoid =?3",nativeQuery = true)
