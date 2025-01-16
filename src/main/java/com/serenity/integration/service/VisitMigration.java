06f23f6a615cdf123c19ac842368376ef46000a9
Bryan Laryea <bry_lar@yahoo.com>
2025-01-16 11:13:38 +0000

    added encounter threads

--- src/main/java/com/serenity/integration/service/VisitMigration.java
+++ src/main/java/com/serenity/integration/service/VisitMigration.java
@@ -45,7 +45,7 @@ public class VisitMigration {
 
 
 public void getPatientsThreads(){
-//String sql ="SELECT count(*) FROM visists where externalsystem='his'";
+String sql ="SELECT count(*) FROM visists where externalsystem='his'";
     int dataSize = 2160000;
     ExecutorService executorService =  Executors.newFixedThreadPool(10);
     try {
@@ -176,7 +176,7 @@ for (int i=0;i<=rounds;i++){
                     + //
                     "VALUES(to_timestamp(?, 'YYYY-MM-DD HH24:MI:SS'),nextval('visits_id_seq'::regclass),uuid(?),?  ,?," +
                     "?,to_timestamp(?, 'YYYY-MM-DD HH24:MI:SS'),to_timestamp(?, 'YYYY-MM-DD HH24:MI:SS'),?,?," +
-                    "uuid(?),?,?,?,?," +
+                    "uuid(?),?,?,uuid(?),?," +
                     "?,TO_DATE(?, 'YYYY/MM/DD'),?,?,?,uuid(?),?,uuid('23f59485-8518-4f4e-9146-d061dfe58175'),'Airport Primary Care')";
     
             System.err.println("Settting Insert values ");
