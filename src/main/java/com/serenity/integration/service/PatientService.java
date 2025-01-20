b5e61ab3598ba42fabad54eb6c776545a1ae3a39
Bryan Laryea <bry_lar@yahoo.com>
2025-01-20 13:07:07 +0000

    added his notes

--- src/main/java/com/serenity/integration/service/PatientService.java
+++ src/main/java/com/serenity/integration/service/PatientService.java
@@ -482,7 +482,7 @@ public class PatientService {
         List<PatientData> patientData = new ArrayList<>();
         // Note: Set<String> mrNumbers is declared but never used
         
-        String sql = "SELECT * FROM patient where patient.mr_number  like 'NMC/%' ORDER BY id";
+        String sql = "SELECT * FROM patient where patient.mr_number NOT  like  'NMC/%' ORDER BY id";
     
             SqlRowSet resultSet = legJdbcTemplate.queryForRowSet(sql);
             while (resultSet.next()) {
