aadb0594d0595c8e77b66e7938b02ecffe2eb7c6
Bryan Laryea <bry_lar@yahoo.com>
2025-01-26 12:52:52 +0000

    added his notesf

--- src/main/java/com/serenity/integration/IntegrationApplication.java
+++ src/main/java/com/serenity/integration/IntegrationApplication.java
@@ -81,7 +81,7 @@ public class IntegrationApplication {
 	///	visitService.getIPDVISITSThreads();
 		//visitService.getlegacyThreads();
 		//visitMigration.getVisitThreads();
-		noteService.truncate();
+		//noteService.truncate();
 		 noteService.getChiefNotes();
 		noteService.cleanData();
 
