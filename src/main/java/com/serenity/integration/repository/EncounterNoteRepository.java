faed42091a71ead3db039867e6e9f60745ca42c2
Bryan Laryea <bry_lar@yahoo.com>
2025-01-27 18:06:40 +0000

    added his notesf

--- src/main/java/com/serenity/integration/repository/EncounterNoteRepository.java
+++ src/main/java/com/serenity/integration/repository/EncounterNoteRepository.java
@@ -16,7 +16,7 @@ public interface EncounterNoteRepository extends  JpaRepository<EncounterNote, S
     @Query(value = "SELECT * FROM encounternote  OFFSET ?1 LIMIT ?2",nativeQuery = true)
     List<EncounterNote> findOffsetData(int offset,int limit);
 
-    @Query(value = "select * from encounternote e  where  practitionerid is not null and visitid is not null order by id OFFSET ?1 LIMIT 1000",nativeQuery = true)
+    @Query(value = "select * from encounternote where practitionerid is not null and visitid is not null and patientid  is not null order by id OFFSET ?1 LIMIT 1000",nativeQuery = true)
     List<EncounterNote> findOffset(int offset);
 
 }
