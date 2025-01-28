86a1ed1caa3416e471e3076efe88a08905b3167e
Bryan Laryea <bry_lar@yahoo.com>
2025-01-28 02:15:07 +0000

    added his notesf

--- src/main/java/com/serenity/integration/service/MedicalRequestService.java
+++ src/main/java/com/serenity/integration/service/MedicalRequestService.java
@@ -352,6 +352,7 @@ Map<String, String> doc = doctorRepository.findHisPractitioners().stream()
                 }
                 List<Encounter> encouter = encounterRepository.findByExternalIdAndAssignedToId(request.getExternalId(), request.getPractitionerId());
                 request.setEncounterId(encouter.get(0).getUuid());
+                request.setVisitId(encouter.get(0).getVisitId());
                // request.setVisitId(UUID.randomUUID().toString());
               ///  Encounter encounter = new Encounter(request, mps.get(set.getString("patient_id")), "his");
              //   Visits visit = new Visits(encounter);
