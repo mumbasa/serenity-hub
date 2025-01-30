534d178599d1c1f8a33824fc737c6f735227a465
Bryan Laryea <bry_lar@yahoo.com>
2025-01-30 22:27:38 +0000

    added his diagnosis

--- src/main/java/com/serenity/integration/service/DiagnosisService.java
+++ src/main/java/com/serenity/integration/service/DiagnosisService.java
@@ -204,11 +204,11 @@ public class DiagnosisService {
         int rows = hisJdbcTemplate.queryForObject(sqlCount, Integer.class);
         logger.info(rows + " number of rows");
         int totalSize = rows;
-        int batches = (totalSize + 10000 - 1) / 10000; // Ceiling division
+        int batches = (totalSize + 1000 - 1) / 1000; // Ceiling division
 
         for (int i = 0; i < batches; i++) {
-            int startIndex = i * 10000;
-            int endIndex = Math.min(startIndex + 10000, totalSize);
+            int startIndex = i * 1000;
+            int endIndex = Math.min(startIndex + 1000, totalSize);
 
             String sqlQuery = """
                           select
@@ -238,7 +238,7 @@ public class DiagnosisService {
                     inner join icd_10_new icd on ccp.icd_id = icd.ID
                     inner join employee_master em on em.Employee_ID = ccp.UserID
                     inner join patient_medical_history pmh on pmh.Transaction_ID = ccp.Transaction_ID
-                    left join doctor_master dm on dm.Doctor_ID = pmh.Doctor_ID  LIMIT ?,10000;
+                    left join doctor_master dm on dm.Doctor_ID = pmh.Doctor_ID  LIMIT ?,1000;
                                             """;
             SqlRowSet set = hisJdbcTemplate.queryForRowSet(sqlQuery, startIndex);
             while (set.next()) {
