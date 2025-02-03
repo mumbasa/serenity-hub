ec44fd56763dadf994a25988aa2c281a2e7448f8
Bryan Laryea <bry_lar@yahoo.com>
2025-02-03 12:06:41 +0000

    added his diagnosis

--- src/main/java/com/serenity/integration/IntegrationApplication.java
+++ src/main/java/com/serenity/integration/IntegrationApplication.java
@@ -113,7 +113,7 @@ public class IntegrationApplication {
 		// noteService.noteThread();
 		// diagnosisService.getNursingDiagnosis();
 		// diagnosisService.getProvisionalDiagnosis();;
-		diagnosisService.provisionalDiagnosisThread();
+		diagnosisService.getProvisionalDiagnosis();
 		//diagnosisService.migrationThread();
 
 		logger.info("finishi.ng import");
